7a6127986a726179154d6c17aa8a9316c82a27a9
Emma <emmagray183@gmail.com>
2020-09-10 17:02:01 +0100

    Changes green stripe text

--- src/components/greenStripe.js
+++ src/components/greenStripe.js
@@ -4,7 +4,12 @@ export default function GreenStripe() {
     return (
         <div class="row green-stripe-container">
         <div class="green-stripe">
-            <p className="green-stripe-info">Laura Lawson is a qualified, mobile therapist who performs a range of holistic massage therapies in the comfort of your own home.</p>
+            <p className="green-stripe-info">Welcome!</p>
+            <p className="green-stripe-info">Holistic Wellbeing was established in 2012, by Laura Lawson.</p>
+            <p className="green-stripe-info">
+            Our Complementary Therapists are fully insured, highly qualified and 
+            committed to providing the very best professional, high-quality treatments.
+            </p>
         </div>
         </div>
     )}

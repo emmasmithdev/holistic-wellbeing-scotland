cbe0e81ef19a5262b7a3e3b2ba23884099ae71ee
Emma <emmagray183@gmail.com>
2020-09-19 17:40:42 +0100

    Pamper parties page

--- src/containers/homeContainer.js
+++ src/containers/homeContainer.js
@@ -19,12 +19,12 @@ export default function HomeContainer() {
             <div class="col-sm-12 col-md-4">
                 <HomeImageQuote image={Reflexology} alt="Feet in a basin of water with flowers."
                 text="'Laura really knows how to work with the body, getting deeply into every muscle. I feel so relaxed, restored and revitalised. Utter bliss.' - Laura P."/>
-                <HomeMassageInfo text="All of our clients are important to us. Everyone is treated as a VIP and receives a high quality treatment."/>
+                <HomeMassageInfo text="All of our clients are important to us. Everyone is treated as a VIP and receives high quality service and expert skills."/>
             </div>
             <div class="col-sm-12 col-md-4">
                 <HomeImageQuote image={EssentialOils} alt="Bottles of essential oils with flowers."
                 text="'Just had the most amazing pregnancy massage ever! Laura is so nice and I felt so at ease, the most relaxed I’ve felt in a while.' - Lauren H."/>
-                <HomeMassageInfo text="We provide everything for the perfect treatment. Sit back and let us look after you."/>
+                <HomeMassageInfo text="We provide the finest Egyptian cotton linen and homemade aromatherapy balm. Sit back and let us look after you."/>
             </div>
         </div>
         <div class="row">
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,11 +1,45 @@
 import React from "react"
 import Layout from "../components/layout" 
+import PamperBanner from "../images/pamperBanner.jpg";
+import PamperPolaroid from "../images/pamperPolaroid.jpg";
+import PamperPolaroid3 from "../images/pamperPolaroid3.jpg";
+import PamperPolaroid2 from "../images/pamperPolaroid2.jpg";
+import PamperPolaroid4 from "../images/pamperPolaroid4.jpg";
+import Prosecco from "../images/prosecco.jpg";
+import OilPestle from "../images/oilPestle.jpg";
+import Champagne from "../images/champagne.jpg";
+
 
 export default function Events() {
   return (
     <Layout>
-       <div class="coming-soon-wrapper" style={{ color: `#a0cf67` }}>
-        <h1>Coming soon...</h1>
+      <div class="events-header">
+        <img src={PamperBanner} alt="Two girls applying face masks" className="pamper-party-banner-image"/>
+        <h2 class="events-heading">Pamper Parties</h2>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-12 polaroid-container" id="left-polaroids">
+          <img src={PamperPolaroid} alt = "Two girls on sofa with cucumbers on their eyes" className="pamper-polaroid-portrait left-polaroid"/>
+          <img src={Champagne} alt = "Champagne glasses clinking" className="pamper-polaroid-portrait left-polaroid"/>
+        </div>
+        <div class="col-lg-6 pamper-party-text">
+          <p>
+          At Holistic Wellbeing we have a range of packages that ensures you experience the ultimate in pampering but without the expensive price tag. 
+          And more importantly - we come to you.</p>  
+          <img src={PamperPolaroid2} alt = "Two girls taking a selfie pouting" className="pamper-polaroid-landscape"/>
+          <p>Whether you want to have a party in the comfort of your own home, at a hotel 
+          or rented accommodation, our fully insured and professional Complementary Therapists will create a haven of tranquillity wherever you are, so that 
+          you can indulge in your very own sanctuary.</p>
+          <img src={OilPestle} alt = "Oil pestle" id="oil-pestle" className="pamper-polaroid-small"/>
+          <img src={Prosecco} alt = "Bottle of prosecco" className="pamper-polaroid-small"/>
+          <p>Our pamper packages & treatments, range from massages to facials, and we will provide some fizz too!</p>
+          <br/>
+          <a href="/contact" className="contact-link">Contact Laura for more information</a>
+        </div>
+        <div class="col-lg-3 col-md-12 polaroid-container">
+          <img src={PamperPolaroid3} alt = "Two girls on the sofa drinking wine and using face cream" className="pamper-polaroid-portrait"/>
+          <img src={PamperPolaroid4} alt = "A girl with a face mask on" className="pamper-polaroid-portrait"/>
+        </div>
     </div>
     </Layout>
   )

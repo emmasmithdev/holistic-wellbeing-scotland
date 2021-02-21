8c2053d2d584c75c7d75843ed837decbad73ed1c
Emma <emmagray183@gmail.com>
2021-02-21 22:04:10 +0000

    Testimonials uodate

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -9,39 +9,39 @@ export default function Testimonials() {
       </div>
        <div class="flex-container">
        <div class="flex-item test">
-          <p class="testimonial">Laura is fantastic. She provides excellent massages for myself and my husband in our home. Laura is warm, friendly, and makes you feel completely at ease. She is very knowledgeable and helps ease your problem areas. I can't recommend her enough. </p>
+          <p class="testimonial">"Laura is fantastic. She provides excellent massages for myself and my husband in our home. Laura is warm, friendly, and makes you feel completely at ease. She is very knowledgeable and helps ease your problem areas. I can't recommend her enough." </p>
           <p class="name">- Kirsty C.</p>
          </div>
          <div class="flex-item test">
-          <p class="testimonial">'Laura really knows how to work with the body, getting deeply into every muscle. I feel so relaxed, restored and revitalised. Utter bliss.'</p>
+          <p class="testimonial">"Laura really knows how to work with the body, getting deeply into every muscle. I feel so relaxed, restored and revitalised. Utter bliss."</p>
           <p class="name">- Laura P.</p>
          </div>
          <div class="flex-item test">
-          <p class="testimonial">I started pre-natal massage with Laura in my second trimester. Laura tailored every session to my (ever changing) needs. I was so relaxed and reassured during every treatment. However what I loved most about Laura is her personable and professional demeanour. I highly recommend her.</p>
+          <p class="testimonial">"I started pre-natal massage with Laura in my second trimester. Laura tailored every session to my (ever changing) needs. I was so relaxed and reassured during every treatment. However what I loved most about Laura is her personable and professional demeanour. I highly recommend her."</p>
           <p class="name">- Gemma C.</p>
          </div>
          <div class="flex-item test">
-          <p class="testimonial">I had a one hour swedish massage on my back and shoulders from Laura after she was recommended by my sister-in-law. She was very friendly and professional. The massage was fantastic. I've had various massages in different spas and this was definitely among the best.</p>
+          <p class="testimonial">"I had a one hour swedish massage on my back and shoulders from Laura after she was recommended by my sister-in-law. She was very friendly and professional. The massage was fantastic. I've had various massages in different spas and this was definitely among the best."</p>
           <p class="name">- John H.</p>
          </div>
          <div class="flex-item test">
-          <p class="testimonial">I just had the most incredible massage from Laura. It was like she was drawn to every knot in my back and the relief was like nothing else.</p>
+          <p class="testimonial">"I just had the most incredible massage from Laura. It was like she was drawn to every knot in my back and the relief was like nothing else."</p>
           <p class="name">- Lauren D.</p>
          </div>
          <div class="flex-item test">
-          <p class="testimonial">Laura is a fab therapist who really listens to her client’s needs. I feel great after every massage.</p>
-          <p class="name">- Laura J.</p>
+          <p class="testimonial">"Laura provided some much needed care to our members at our annual conference.  She provided a friendly and professional service and we look forward to working with her again in the future."</p>
+          <p class="name">- Royal Blind.</p>
          </div>
          <div class="flex-item test">
-          <p class="testimonial">Fantastic pregnancy massage and reflexology. Laura eased all my aches and pains and I slept better than I have in months. </p>
+          <p class="testimonial">"Fantastic pregnancy massage and reflexology. Laura eased all my aches and pains and I slept better than I have in months." </p>
           <p class="name">- Sarah B.</p>
          </div>
          <div class="flex-item test">
-          <p class="testimonial">Just had an amazing massage from Laura! She was very knowledable and knew exactly where all my knots were. She created an amazing ambiance right in my home. The massage was so relaxing and I look forward to her next visit! </p>
+          <p class="testimonial">"Just had an amazing massage from Laura! She was very knowledable and knew exactly where all my knots were. She created an amazing ambiance right in my home. The massage was so relaxing and I look forward to her next visit!" </p>
           <p class="name">- Lauren W.</p>
          </div>  
          <div class="flex-item test">
-          <p class="testimonial">What an experience! After having a sports back injury few months ago and explained to Laura what happend she found and eased every nook and cranny! The differance was amazing! Every knot and bit of tension eased in one session! </p>
+          <p class="testimonial">"What an experience! After having a sports back injury few months ago and explained to Laura what happend she found and eased every nook and cranny! The differance was amazing! Every knot and bit of tension eased in one session!" </p>
           <p class="name">- Craig M.</p>
          </div>   
     </div>

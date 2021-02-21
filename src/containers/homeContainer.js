eb89298e3147b011f848e9c4a5cc1c645f1a35e4
Emma <emmagray183@gmail.com>
2021-02-21 20:08:30 +0000

    Testimonials page

--- src/containers/homeContainer.js
+++ src/containers/homeContainer.js
@@ -12,18 +12,15 @@ export default function HomeContainer() {
         <div class="home-container">
         <div class="row">
             <div class="col-sm-12 col-md-4">
-                <HomeImageQuote image={AndBreathe} alt="A wall of plants with And Breathe written in neon."
-                text="'Laura listened to my concerns carefully and really worked to ease off the stiffness and tension I had been experiencing.' - Charlotte H."/>
+                <HomeImageQuote image={AndBreathe} alt="A wall of plants with And Breathe written in neon."/>
                 <HomeMassageInfo text="With Holistic Wellbeing, you're in safe hands. Every treatment is tailored specifically to your needs."/>
             </div>
             <div class="col-sm-12 col-md-4">
-                <HomeImageQuote image={Reflexology} alt="Feet in a basin of water with flowers."
-                text="'Laura really knows how to work with the body, getting deeply into every muscle. I feel so relaxed, restored and revitalised. Utter bliss.' - Laura P."/>
+                <HomeImageQuote image={Reflexology} alt="Feet in a basin of water with flowers."/>
                 <HomeMassageInfo text="All of our clients are important to us. Everyone is treated as a VIP and receives high quality service and expert skills."/>
             </div>
             <div class="col-sm-12 col-md-4">
-                <HomeImageQuote image={EssentialOils} alt="Bottles of essential oils with flowers."
-                text="'Just had the most amazing pregnancy massage ever! Laura is so nice and I felt so at ease, the most relaxed I’ve felt in a while.' - Lauren H."/>
+                <HomeImageQuote image={EssentialOils} alt="Bottles of essential oils with flowers."/>
                 <HomeMassageInfo text="We provide the finest Egyptian cotton linen and homemade aromatherapy balm. Sit back and let us look after you."/>
             </div>
         </div>

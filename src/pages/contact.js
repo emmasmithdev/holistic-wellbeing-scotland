fad990aa5b1088aba0667dfa4a6e29ecc3e6d5ba
Emma <emmagray183@gmail.com>
2021-02-21 21:08:41 +0000

    Contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,15 @@ import Layout from "../components/layout"
 export default function Contact() {
   return (
     <Layout>
-     <div class="coming-soon-wrapper" style={{ color: `#a0cf67` }}>
-        <h1>Coming soon...</h1>
+     <div class="contact-container">
+       <h2>Contact Us</h2>
+     <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <p><input type="hidden" name="form-name" value="contact" /><br/></p>
+      <p><input type="text" name="name" placeholder="Name" /><br/></p>
+      <p><input type="email" name="email" placeholder="Email" /></p>
+      <textarea name="message" placeholder="Message"></textarea>
+      <p><button class="search-button" type="submit">Send</button></p>
+    </form>
     </div>
     </Layout>
   )

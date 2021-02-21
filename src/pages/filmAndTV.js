cf2431f1a9810ac5b6549fc1dcc63d3aea893a6a
Emma <emmagray183@gmail.com>
2021-02-21 15:50:54 +0000

    Film and TV page and responsiveness

--- src/pages/filmAndTV.js
+++ src/pages/filmAndTV.js
@@ -1,12 +1,35 @@
 import React from "react"
 import Layout from "../components/layout" 
+import FilmBanner from "../images/filmBanner.png";
+import FilmSet from "../images/filmSet.jpg";
+import FilmSet2 from "../images/filmSet2.jpg";
+import GreenStripe from "../components/greenStripe"
 
 export default function FilmAndTV() {
   return (
     <Layout>
-       <div class="coming-soon-wrapper" style={{ color: `#a0cf67` }}>
-        <h1>Coming soon...</h1>
-    </div>
+      <div class="corporate-header">
+        <img src={FilmBanner} alt="Movie set" className="film-banner-image"/>
+        <h2 class="film-heading">Film and Television Production</h2>
+      </div>
+      
+      <div class="film-container">
+        <div class="film-image">
+          <img src={FilmSet2} alt="Film set" className="film-image"/>
+        </div>
+        <div class="film-text">
+          <h3 class="film-subheading">From the Set to the Wrap Party</h3>
+          <p class="film-info">With minimal equipment and space needed, we can set up at any venue, 
+            - on set or on location. We recommend a quiet room or trailer as this often 
+            provides a more peaceful experience away from the action. However a massage station can be set up in a 
+            quiet corner where the therapist can easily treat cast and crew. </p>
+            <p>At post-production celebrations, massage therapy can be an excellent way to reward your team for their 
+              effort and hard work after months of filming.</p>
+        </div>
+        <div class="film-image">
+          <img src={FilmSet} alt="Film set" className="film-image"/>
+        </div>
+      </div>
     </Layout>
   )
 }
